bc7cadef1c56e3bf56011d04202acdd3b0a51375
Muralidharan <muralivijay2804@gmail.com>
2022-06-28 14:28:15 +0530

    commit

--- calculation/settings.py
+++ calculation/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-4mbpsd=vcj=)z2f&$odh)n#xefpr5hnf_5&%+m8ybpixgds&91
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','gpacalculation.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','cgpa-calculation.herokuapp.com']
 
 
 # Application definition
